3e271b399abab44e84c21a301971371679d9a5c7
matee-safi <matiullahsafibbc@gmail.com>
2023-06-24 20:21:43 +0430

    add tests and validations

--- Gemfile
+++ Gemfile
@@ -72,3 +72,8 @@ group :test do
 end
 
 gem 'rubocop', '>= 1.0', '< 2.0'
+
+group :development, :test do  
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_numericality_of(:posts_counter).only_integer.is_greater_than_or_equal_to(0) }
-  end
-
-  describe 'associations' do
+  describe 'Associations' do
     it { should have_many(:posts).dependent(:destroy) }
     it { should have_many(:comments).dependent(:destroy) }
     it { should have_many(:likes).dependent(:destroy) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_numericality_of(:posts_counter).only_integer.is_greater_than_or_equal_to(0) }
   end
 
   describe '#recent_posts' do
