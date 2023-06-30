74c230785e3b6f47899aacb47089af2d33286ecd
matee-safi <matiullahsafibbc@gmail.com>
2023-06-30 09:32:11 +0430

    fix linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-    describe "GET #index each user posts" do
-        before :each do
-            get '/users/1/posts'
-        end
-        it "returns http success" do
-            expect(response).to have_http_status(:success)
-        end
-        it "checks if the correct template was rendered" do
-            expect(response).to render_template(:index)
-        end
-        it "checks if the response body includes the correct placeholder text" do
-            expect(response.body).to include("Posts")
-        end
+RSpec.describe 'Posts', type: :request do
+  describe 'GET #index each user posts' do
+    before :each do
+      get '/users/1/posts'
     end
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'checks if the correct template was rendered' do
+      expect(response).to render_template(:index)
+    end
+    it 'checks if the response body includes the correct placeholder text' do
+      expect(response.body).to include('Posts')
+    end
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-    describe "GET #index" do
-        before :each do
-            get users_path
-        end
-        it "returns http success" do
-            expect(response).to have_http_status(:success)
-        end
-        it "checks if the correct template was rendered" do
-            expect(response).to render_template(:index)
-        end
-        it "checks if the response body includes the correct placeholder text" do
-            expect(response.body).to include("Users")
-        end
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    before :each do
+      get users_path
     end
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'checks if the correct template was rendered' do
+      expect(response).to render_template(:index)
+    end
+    it 'checks if the response body includes the correct placeholder text' do
+      expect(response.body).to include('Users')
+    end
+  end
 end
