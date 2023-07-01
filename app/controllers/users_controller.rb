29d16524e29c691ff154801cff7e7d0d70a80ba4
matee-safi <matiullahsafibbc@gmail.com>
2023-07-01 12:30:49 +0430

    Create views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:user_id])
+    @user = User.find_by(id: params[:id])
+    @recent_posts = @user.recent_posts
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end
+
+  def posts_counter
+    posts.count
+  end
 end
