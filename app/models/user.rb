6cb00dd157b421165defbbc292024b44b0fb8477
matee-safi <matiullahsafibbc@gmail.com>
2023-07-16 02:18:39 +0430

    add required changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  :confirmable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: :author_id, dependent: :destroy
