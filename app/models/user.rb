e964bad8b75052e8e4c7474aa1a7f448fe841fdd
matee-safi <matiullahsafibbc@gmail.com>
2023-07-16 02:20:21 +0430

    fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  :confirmable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: :author_id, dependent: :destroy
